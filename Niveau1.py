53553a9f7fa8cf33ea3c2395b541ce92ddad3631
BilalB76 <83305301+BilalB76@users.noreply.github.com>
2021-05-03 03:11:47 +0200

    Update Niveau1.py

--- Niveau1.py
+++ Niveau1.py
@@ -1,7 +1,6 @@
 def Niveau1():
     print("Bienvenue sur le célèbre jeu du juste prix, tu dois deviner le prix auquel je pense, il se situe entre 1 et 100")
     import random
-    x = 14
     nombre_choisi = random.randint(1,100)
     nombre = int(input("Entrez une proposition : "))
     if (nombre < nombre_choisi):
